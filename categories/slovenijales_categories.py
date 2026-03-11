b3f99c9cdccb0df282c4c8319457f94963136d06
tok805048-oss <tok805048@gmail.com>
2026-03-11 09:01:12 +0100

    Add Slovenijales categories for construction materials

---
+++ categories/slovenijales_categories.py
@@ -0,0 +1,9 @@
+SLOVENIJALES_CATEGORIES = {
+    "Gradnja": [
+        "https://trgovina.slovenijales.si/gradbeni-material/izolacije-in-folije",
+        "https://trgovina.slovenijales.si/lesni-materiali/gradbene-plosce-in-les",
+        "https://trgovina.slovenijales.si/lesni-materiali/lepljene-plosce",
+        "https://trgovina.slovenijales.si/ploskovni-materiali/iverne-plosce",
+        "https://trgovina.slovenijales.si/ploskovni-materiali/vlaknene-plosce",
+    ]
+}
