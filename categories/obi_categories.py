b6baf40227c433136d33f42480e196ef1f037d56
tok805048-oss <tok805048@gmail.com>
2026-03-11 08:45:45 +0100

    Create obi_categories.py

---
+++ categories/obi_categories.py
@@ -0,0 +1,9 @@
+OBI_CATEGORIES = {
+    "Gradnja": [
+        "https://www.obi.si/c/gradnja-877/gradbeni-materiali-175/izolacijski-material-233",
+        "https://www.obi.si/c/gradnja-877/gradbeni-materiali-175/omet-malta-in-cement-619",
+        "https://www.obi.si/c/gradnja-877/gradbeni-materiali-175/suha-gradnja-764",
+        "https://www.obi.si/c/gradnja-877/ploscice-308/talne-ploscice-1150",
+        "https://www.obi.si/c/gradnja-877/ureditev-okolice-336/tlakovci-608",
+    ]
+}
