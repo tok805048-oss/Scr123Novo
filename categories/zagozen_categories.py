e342389295ae54d6cbfbda2d1ad9cac8e5e5728c
tok805048-oss <tok805048@gmail.com>
2026-03-11 09:24:17 +0100

    Add ZAGOZEN_CATEGORIES with relevant URLs

---
+++ categories/zagozen_categories.py
@@ -0,0 +1,9 @@
+ZAGOZEN_CATEGORIES = {
+    "Vodovod": [
+        "https://eshop-zagozen.si/vodovod",
+        "https://eshop-zagozen.si/kanalizacija",
+        "https://eshop-zagozen.si/kanalizacija/kanalizacijske-cevi-in-fazoni",
+        "https://eshop-zagozen.si/kanalizacija/kanalizacijski-jaski-in-oprema/peskolovi",
+        "https://eshop-zagozen.si/vodovod/cevi-in-fitingi",
+    ]
+}
