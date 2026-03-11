eea45a86350268c2b4da15254bec2a370c2554b0
tok805048-oss <tok805048@gmail.com>
2026-03-11 09:17:05 +0100

    Add TEHNOLES_CATEGORIES with construction materials

---
+++ categories/tehnoles_categories.py
@@ -0,0 +1,9 @@
+TEHNOLES_CATEGORIES = {
+    "Gradbeni material": [
+        "https://www.tehnoles.si/izolacije-c-48.aspx",
+        "https://www.tehnoles.si/suhomontazni-material-c-17.aspx",
+        "https://www.tehnoles.si/lepila-in-kiti-c-32.aspx",
+        "https://www.tehnoles.si/barve-laki-in-premazi-c-31.aspx",
+        "https://www.tehnoles.si/vodovod-c-151.aspx",
+    ]
+}
