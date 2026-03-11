69827ad2eb7328e5cfac14ffaacad49d0b236068
tok805048-oss <tok805048@gmail.com>
2026-03-11 08:34:12 +0100

    Add MERKUR_CATEGORIES for construction links

---
+++ categories/merkur_categories.py
@@ -0,0 +1,9 @@
+MERKUR_CATEGORIES = {
+    "Gradnja": [
+        "https://www.merkur.si/gradnja/termoizolacije/estrudirani-polistiren-xps/",
+        "https://www.merkur.si/gradnja/termoizolacije/stiropor/",
+        "https://www.merkur.si/gradnja/termoizolacije/steklena-volna/",
+        "https://www.merkur.si/gradnja/hidroizolacije/bitumenski-trakovi-in-premazi/bitumenski-trakovi/",
+        "https://www.merkur.si/gradnja/osnovni-gradbeni-izdelki-in-les/osb-in-lsb-plosce/",
+    ]
+}
